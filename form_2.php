90f1b2cfdb2acb416442a37b5a9ec52a0618adbc
donny_444 <104888835+donny444@users.noreply.github.com>
2023-04-15 20:10:33 +0700

    Add files via upload

---
+++ form_2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <form action="save.php" method="get">
+        ค้นหา
+        <input type="text" name="search" required>
+        <button type="submit">ค้นหาข้อมูล</button>
+    </form>
+</body>
+</html>
---
+++ save.php
@@ -0,0 +1,11 @@
+<?php
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
+echo "<hr>";
+echo "<pre>";
+var_dump($_GET);
+echo "</pre>";
+echo "<hr>";
+echo "คุณกำลังค้นหา ".$_GET["search"];
+?>
