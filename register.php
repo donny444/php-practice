e47601b8f376cbe9b5d56c351b7536146cb1b695
donny_444 <104888835+donny444@users.noreply.github.com>
2023-04-15 12:58:43 +0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <h1>Register form</h1>
+    <form action="" method="">
+        <label>Username: </label>
+        <input type="text" name="username" required>
+        <br>
+        <label>Password: </label>
+        <input type="password" name="password" required>
+        <br>
+        <label>Name: </label>
+        <input type="text" name="member_name" required>
+        <br>
+        <label>Phone: </label>
+        <input type="text" name="phone" required>
+        <br>
+        <label>Email: </label>
+        <input type="email" name="email" required>
+        <br>
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
