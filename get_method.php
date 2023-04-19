34e2e96d1b21ec058834330f07a99f64764e9069
donny_444 <104888835+donny444@users.noreply.github.com>
2023-04-19 23:50:52 +0700

    Add files via upload

---
+++ get_method.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <h1>Computer Survey</h1>
+    <form id="survey" action="save_get.php" method="get">
+        <label form="survey">How old are you </label>
+        <input type="number" name="age" form="survey" required>
+        <br>
+        <label form="survey">What you interesting in</label>
+        <select form="survey">
+            <option>Front-End Development</option>
+            <option>Back-End Development</option>
+            <option>Data Science</option>
+            <option>Cyber Security</option>
+        </select>
+        <input type="submit" value="Submit" form="survey">
+    </form>
+</body>
+</html>
---
+++ save_post.php
@@ -0,0 +1,9 @@
+<?php
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+    echo "<hr>";
+    echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";
+?>
